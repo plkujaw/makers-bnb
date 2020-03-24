678817023773ed1d8e6a8923d32b43be57e0d8f9
plkujaw <jawor.jakub@gmail.com>
2020-03-24 12:04:21 +0000

    added description column to spaces table
    Co-Authored-By: Kate Haffenden <naeglinghaff@users.noreply.github.com>
    Co-Authored-By: null <jamesoddy8@users.noreply.github.com>
    Co-Authored-By: null <artemis-p@users.noreply.github.com>
    Co-Authored-By: George Swarbrick <Swarbzz@users.noreply.github.com>
    Co-Authored-By: Karla Gardiner <KG700@users.noreply.github.com>
    Co-Authored-By: Kuba <plkujaw@users.noreply.github.com>

--- db/migrations/02_split_address_column.sql
+++ db/migrations/02_split_address_column.sql
@@ -1,4 +1,5 @@
 ALTER TABLE "public"."spaces" RENAME COLUMN "address" TO "street_address";
-ALTER TABLE "public"."spaces" ADD COLUMN "country" varchar(50)
-ALTER TABLE "public"."spaces" ADD COLUMN "city" varchar(50)
-ALTER TABLE "public"."spaces" ADD COLUMN "postcode" varchar(10)
+ALTER TABLE "public"."spaces" ADD COLUMN "country" varchar(50);
+ALTER TABLE "public"."spaces" ADD COLUMN "city" varchar(50);
+ALTER TABLE "public"."spaces" ADD COLUMN "postcode" varchar(10);
+ALTER TABLE "public"."spaces" ADD COLUMN "description" varchar(500);
