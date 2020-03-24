af9e2871ac375cffbcaaf21464865de0b4ba5a33
Karla <karla.gardiner@gmail.com>
2020-03-24 14:58:24 +0000

    Linked spaces table to users table

--- db/migrations/03_create_users_table.sql
+++ db/migrations/03_create_users_table.sql
@@ -1,1 +1,2 @@
-CREATE TABLE "public"."users" ("id" serial,"name" varchar(60),"email" varchar(60), "password" varchar(60));
+CREATE TABLE "public"."users" ("id" serial PRIMARY KEY,"name" varchar(60),"email" varchar(60), "password" varchar(60));
+ALTER TABLE "public"."spaces" ADD COLUMN "owner_id" INTEGER REFERENCES users (id);
