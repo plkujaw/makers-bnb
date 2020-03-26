17e910e7e17c1d1571d993ce96a403cb49646bad
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-26 18:16:07 +0000

    Added patch request to booking confirmations

--- lib/booking_received.rb
+++ lib/booking_received.rb
@@ -1,7 +1,7 @@
 class BookingReceived
 
   def self.all(owner_id)
-    @bookings_received = ActiveRecord::Base.connection.execute("SELECT s.name, s.price, s.description, s.street_address, s.city, s.country, s.postcode, b.confirmation, b.booking_start, b.booking_end FROM spaces s LEFT JOIN bookings b ON s.id = b.space_id WHERE s.owner_id = #{owner_id}")
+    @bookings_received = ActiveRecord::Base.connection.execute("SELECT s.name, s.price, s.description, s.street_address, s.city, s.country, s.postcode, b.id booking_id, b.confirmation, b.booking_start, b.booking_end FROM spaces s LEFT JOIN bookings b ON s.id = b.space_id WHERE s.owner_id = #{owner_id}")
 
     @bookings_received.map do |request|
       # p request
@@ -15,14 +15,15 @@ class BookingReceived
         space_post_code: request['postcode'],
         booking_confirmation: request['confirmation'],
         booking_start_date: request['booking_start'],
-        booking_end_date: request['booking_end']
+        booking_end_date: request['booking_end'],
+        booking_id: request['booking_id']
       )
     end
   end
 
-  attr_reader :space_name, :space_price, :space_description, :space_street_address, :space_city, :space_country, :space_post_code, :booking_confirmation, :booking_start_date, :booking_end_date
+  attr_reader :space_name, :space_price, :space_description, :space_street_address, :space_city, :space_country, :space_post_code, :booking_confirmation, :booking_start_date, :booking_end_date, :booking_id
 
-  def initialize(space_name:, space_price:, space_description:, space_street_address:, space_city:, space_country:, space_post_code:, booking_confirmation:, booking_start_date:, booking_end_date:)
+  def initialize(space_name:, space_price:, space_description:, space_street_address:, space_city:, space_country:, space_post_code:, booking_confirmation:, booking_start_date:, booking_end_date:, booking_id:)
     @space_name = space_name
     @space_price = space_price
     @space_description = space_description
@@ -33,6 +34,7 @@ class BookingReceived
     @booking_confirmation = booking_confirmation
     @booking_start_date = booking_start_date
     @booking_end_date = booking_end_date
+    @booking_id = booking_id
   end
 
 end
